854f10efa965703ab0dd96f2624ab8271e06a1d7
Milan Dierick <milan.dierick@gmail.com>
2020-08-01 15:31:05 +0200

    Updated 'build_skia.bat' so that it works out of the box, also added in additional line to update repo dependencies in case the user hasn't done this manually.

--- SkiaViki/build_skia.bat
+++ SkiaViki/build_skia.bat
@@ -1,4 +1,7 @@
 REM Run this in a CMD, not Powershell
 REM TODO: Rename Static to Dynamic, since we're using DLLs...
+cd %~dp0Dependencies\skia
+REM ninja: warning: bad deps log signature or version; starting over
+python2 tools/git-sync-deps
 bin\gn gen out/Static --args="is_official_build=false is_component_build=true extra_cflags_cc=[\"/MDd\"] extra_ldflags=[ \"/DEBUG:FULL\" ] extra_cflags=[\"/MDd\"]"
 ninja -C out/Static
