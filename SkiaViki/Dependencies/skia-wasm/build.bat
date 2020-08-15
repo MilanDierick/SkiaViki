46de79dcb5451db7abcdcca2ec9c9901bcb8c5b9
Peter Verswyvelen <bugfact@gmail.com>
2020-08-15 15:22:41 +0200

    WIP: Fixes to build skia wasm, compiling now, and generated several .a files for linking.
    Next step is to write a minimal skia test program, e.g. drawing to a CPU canvas backend, and printing the pixels to the screen...

--- SkiaViki/Dependencies/skia-wasm/build.bat
+++ SkiaViki/Dependencies/skia-wasm/build.bat
@@ -1,16 +1,18 @@
 @echo off
 
+cd /d %~dp0
+
 docker build --target ubuntu -t sts/skia-wasm-ubuntu .
 if errorlevel 1 goto :error
 
 docker build --target source -t sts/skia-wasm-source .
 if errorlevel 1 goto :error
 
-REM docker build --target build -t sts/skia-wasm-build .
-REM if errorlevel 1 goto :error
+docker build --target build -t sts/skia-wasm-build .
+if errorlevel 1 goto :error
 
-REM docker run --rm --name skia-wasm --volume %cd%:/work sts/skia-wasm-build /work/compile.sh
-REM if errorlevel 1 goto :error
+docker run --rm --name skia-wasm --volume %cd%:/work sts/skia-wasm-build /work/compile.sh
+if errorlevel 1 goto :error
 
 REM docker build --target build -t sts/skia-wasm-build .
 REM if errorlevel 1 goto :error
